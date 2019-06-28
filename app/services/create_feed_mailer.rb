721b985fb95d6055ddb8f6aa25046251e7bb6d46
Shane <shane.boyar@gmail.com>
2019-06-28 16:54:31 -0400

    basic mailer service

---
+++ app/services/create_feed_mailer.rb
@@ -0,0 +1,38 @@
+class CreateFeedMailer
+  def initialize(user)
+    @user = user
+    @lob = Lob::Client.new(api_key: Rails.application.secrets.lob_api_key)
+    @filename = "#{Rails.root}/tmp/mail/#{@user.name.parameterize}.html"
+  end
+
+  def create
+    view = ActionView::Base.new(ActionController::Base.view_paths, {
+      user: @user,
+      friends: @user.following.includes(:posts),
+      posts: @user.feed.includes(:user)
+    })
+
+    content = view.render(file: "mailers/feed_mailer.html.erb")
+
+    file = File.open(@filename, 'w+')
+    file << content
+    file.close
+  end
+
+  def send
+    @lob.letters.create(
+      description: "Demo Letter",
+      to: {
+        name: "Test User",
+        address_line1: "185 Berry St",
+        address_line2: "# 6100",
+        address_city: "San Francisco",
+        address_state: "CA",
+        address_zip: "94107"
+      },
+      from: "adr_f05e22b1c577b098",
+      file: File.open(@filename),
+      color: false
+    )
+  end
+end
